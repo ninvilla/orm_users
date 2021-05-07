332c987144436295546e172d285405c7e7637011
Nina Villarreal <ninavillarreal82@yahoo.com>
2021-05-07 17:23:33 -0500

    first commit

---
+++ users_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+def index(request):
+    context = {
+        'users': User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+
+def create(request):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    email = request.POST['email']
+    age = request.POST['age']
+
+    User.objects.create(first_name=first_name, last_name=last_name, email=email, age=age)
+
+    return redirect('/')
+
+
